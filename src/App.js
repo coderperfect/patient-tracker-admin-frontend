b6dc171e60da5a9c024bb2692fcd750e4f1c33c9
coderperfect <mrityunjoynath38@gmail.com>
2021-02-26 16:54:30 +0530

    Menu Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+import {Route, Switch} from 'react-router-dom';
+import MenuComponent from './components/menucomponent';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: true,
+      user: null
+    }
+  }
+
+  logout = () => {
+    this.setState({
+      isLoggedIn: false,
+      user: null
+    })
+  }
+
+  login = (user) => {
+    this.setState({
+      isLoggedIn: true,
+      user: user
+    })
+  }
+
+
+ render() {
+    return (
+      <div className="App">
+        <MenuComponent isLoggedIn={this.state.isLoggedIn} logout={this.logout}/>
+        
+        {/* <Switch>
+          <Route 
+            path="/" exact
+            render={
+              this.state.isLoggedIn
+              ? (props) => <CompaniesListComponent {...props} isLoggedIn={this.state.isLoggedIn} user={this.state.user}/>
+              : (props) => <LoginComponent {...props} login={this.login}/>
+            }
+          />
+        </Switch> */}
+      </div>
+    );
+  }
 }
 
 export default App;
